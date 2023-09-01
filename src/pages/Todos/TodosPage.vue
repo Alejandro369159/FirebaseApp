66238ff6d0af946136366dcfa0a8a7fe5b2444af
Alejandro Sauqué Pérez <alejandro.sauqueprz@uanl.edu.mx>
2023-09-01 10:43:41 -0600

    Se agrega página de todos y se empieza a desarrollar el navbar para usar signOut

---
+++ src/pages/Todos/TodosPage.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { signOut } from 'firebase/auth'
+import { auth } from '@/firebase'
+</script>
+
+<template>
+  <nav>
+    <button>Cerrar Sesion</button>
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  display: flex;
+  padding: 15px 10px;
+  background-color: whitesmoke;
+  justify-content: end;
+}
+</style>
